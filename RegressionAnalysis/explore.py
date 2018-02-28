c879d284aef9c161ccdf1b6922ec121bda3d8ee4
Thomas Caudrelier <tazukay@hotmail.fr>
2018-02-28 20:04:06 +0100

    [RegressionAnalysis]Add quadratic errors

--- RegressionAnalysis/explore.py
+++ RegressionAnalysis/explore.py
@@ -7,19 +7,20 @@ lumi = data[:,0]
 volt = data[:,1]
 mA = data[:,2]
 
-fig = plt.figure()
-plt.plot(volt,lumi,'+')
-plt.xlabel('volt')
-plt.ylabel('lumi')
-plt.show()
+# fig = plt.figure()
+# plt.plot(volt,lumi,'+')
+# plt.xlabel('volt')
+# plt.ylabel('lumi')
+# plt.show()
 
-plt.figure()
-plt.plot(mA,lumi,'+')
-plt.xlabel('mA')
-plt.ylabel('lumi')
-plt.show()
+# plt.figure()
+# plt.plot(mA,lumi,'+')
+# plt.xlabel('mA')
+# plt.ylabel('lumi')
+# plt.show()
 
-n, = lumi.shape
+Y = lumi
+n, = Y.shape
 
 # Parameter estimator for the volt parameter only
 # Regression model: lum_i = Θ_0 + x_i(1) Θ_1 + E_i
@@ -27,8 +28,9 @@ Xvolt = np.matrix(np.concatenate((
         		np.ones((n,1)),
         		volt.reshape((n,1)),
         	), axis=1))
-thetaVolt = np.multiply(np.linalg.inv(Xvolt.T*Xvolt)*Xvolt.T, lumi) # (Xt*X)^(-1)*Xt*y
+thetaVolt = np.multiply(np.linalg.inv(Xvolt.T*Xvolt)*Xvolt.T, Y) # (Xt*X)^(-1)*Xt*y
 lumi_predictor_volt = Xvolt*thetaVolt
+quadratic_error_volt = (Y - Xvolt*thetaVolt).T * (Y - Xvolt*thetaVolt)
 
 # Parameter estimator for the mA parameter only
 # Regression model: lum_i = Θ_0 + x_i(2) Θ_2 + E_i
@@ -36,8 +38,10 @@ XmA = np.matrix(np.concatenate((
         		np.ones((n,1)),
         		mA.reshape((n,1)),
         	), axis=1))
-thetamA = np.multiply(np.linalg.inv(XmA.T*XmA)*XmA.T, lumi) # (Xt*X)^(-1)*Xt*y
+thetamA = np.multiply(np.linalg.inv(XmA.T*XmA)*XmA.T, Y) # (Xt*X)^(-1)*Xt*y
 lumi_predictor_mA = XmA*thetamA
+quadratic_error_mA = (Y - XmA*thetamA).T * (Y - XmA*thetamA)
+
 
 # Parameter estimator for all the parameters
 # Regression model: lum_i = Θ_0 + x_i(1) Θ_1 + x_i(2) Θ_2 + E_i
@@ -46,5 +50,6 @@ X = np.matrix(np.concatenate((
         		volt.reshape((n,1)),
         		mA.reshape((n,1))
         	), axis=1))
-theta = np.multiply(np.linalg.inv(X.T*X)*X.T, lumi) # (Xt*X)^(-1)*Xt*y
+theta = np.multiply(np.linalg.inv(X.T*X)*X.T, Y) # (Xt*X)^(-1)*Xt*y
 lumi_predictor = X*theta
+quadratic_error = (Y - X*theta).T * (Y - X*theta)
